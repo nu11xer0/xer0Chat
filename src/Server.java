01d6fa3258f807a022540c6c98ae0bcd2217258d
Aaron Biros <soribnoraa@gmail.com>
2017-05-24 22:16:53 -0400

    Setting things up.

---
+++ src/Server.java
@@ -0,0 +1,17 @@
+package src;
+/*
+ * 
+ * 
+ * 
+ */
+public class Server 
+{
+	/**
+	 * @param args 			
+	 */
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+}
