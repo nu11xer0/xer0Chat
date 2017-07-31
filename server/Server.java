ad18b42a453fd88f095e187a9f96335e379a06bd
Aaron <soribnoraa@gmail.com>
2017-07-31 19:37:13 -0400

    fthoi

--- server/Server.java
+++ server/Server.java
@@ -7,54 +7,65 @@ package server;
  */
 
 import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 
-public class Server 
-{		
-	
+public class Server{	
 	private static ConHandler handler;
 	
 
-	public static void main(String[] args) 
-	{
-		Scanner consoleInput = new Scanner(System.in);
+	public static void main(String[] args){
 		int choice = 0;
 		
 		//System.out.println("[+] In class \"Server\" method \"main\"");
 		
-		try 
-		{ handler = new ConHandler(); }
-		catch(Exception e)
-		{
+		try { 
+			handler = new ConHandler(); 
+		}
+		catch(Exception e){
 			e.printStackTrace();
 			System.exit(1);
-		}		
+		}	
 		
-		while(true)
-		{
-			printMenu();
-			choice = consoleInput.nextInt();
+		printMenu();
+		
+		while(true){	
+			Scanner consoleInput = new Scanner(System.in);
 			
-			switch(choice)
-			{
-				case 99: System.out.println("[+] Exiting...");
-						 try 
-						 { handler.killSock(); } 
-						 catch (IOException e) 
-						 {	e.printStackTrace(); }
-						 consoleInput.close();
-						 System.exit(0); 
-						 break;
-						 
-				default: System.out.println("[+] Ass sphincter says \"What\"?");
-						 break;
+			try{				
+				choice = consoleInput.nextInt();
+				consoleInput.nextLine();
+														
+				switch(choice){
+					case 99: System.out.println("[+] Exiting...");
+							 try{ 
+								 if(handler.killSock()){
+									 System.out.println("[+] Server shutdown successfully. Bye!");
+								 }
+								 else{
+									 System.err.println("[!] Something went wrong while stopping the server.");
+								 }
+							 } 
+							 catch (IOException e) 
+							 {	e.printStackTrace(); }
+							 consoleInput.close();
+							 System.exit(0); 
+							 break;
+							 
+					default: System.out.println("[+] Invalid input, try again.\n");
+							 printMenu();
+							 break;
+				}				
+			}
+			catch(InputMismatchException e){
+				 System.out.println("[+] Ass sphincter says \"What\"?\n");
+				 printMenu();
 			}
 		}		
 	}//end method main
 	
-	public static void printMenu()
-	{
+	public static void printMenu(){
 		System.out.printf("%s%n%n%s%n%n%s",
 						  "_-= xeroChat Server Console =-_",
 						  "type 99 <enter> to quit.",
