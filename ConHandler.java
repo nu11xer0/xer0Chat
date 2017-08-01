3c47afc7e89c83eb1a95592ce1069a0a0bbf2b88
Aaron <soribnoraa@gmail.com>
2017-08-01 01:27:03 -0400

    Updated some code
    Added the ability to specify a port (-p ###).

--- ConHandler.java
+++ ConHandler.java
@@ -1,12 +1,3 @@
-package server;
-
-/*
- *  FIXIT NOTES
- * null pointer issue in RCV_Thread *fixed
- * IndexOutOfBoundsException in RCV_Thread:123 *fixed
- * 
- * */
-
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.IOException;
@@ -15,28 +6,37 @@ import java.net.Socket;
 import java.net.ServerSocket;
 
 public class ConHandler{
-	private final static int PORT = 4444;
+	private static int defaultPort = 4444;
 	private ServerSocket srvSock;
-	private ArrayList<Socket> conArray = new ArrayList<Socket>(); 
+	private ArrayList<Socket> conArray = new ArrayList<Socket>();
 	private ArrayList<String> unameArray = new ArrayList<String>(); //not implemented yet...
-	private LST_Thread lThread;	
+	private LST_Thread lThread;
 	private Thread x;
-	
-	public ConHandler() throws IOException{		
-		srvSock = new ServerSocket(PORT);
-				
+
+	public ConHandler() throws IOException{
+		srvSock = new ServerSocket(defaultPort);
+
 		// Create and start the listen thread
-		lThread = new LST_Thread();		
+		lThread = new LST_Thread();
 		x = new Thread(lThread);
-		x.start();	
-	}	
+		x.start();
+	}
 	
+	public ConHandler(int port) throws IOException{
+		srvSock = new ServerSocket(port);
+
+		// Create and start the listen thread
+		lThread = new LST_Thread();
+		x = new Thread(lThread);
+		x.start();
+	}
+
 	public Boolean killSock() throws IOException{
 		if(!srvSock.isClosed()){
 			for(int i = 0; i < conArray.size(); i++){
 				conArray.get(i).close();
 			}
-			
+
 			srvSock.close();
 			System.out.println("\n[+] Server Socket closed.");
 			return(true);
@@ -46,48 +46,48 @@ public class ConHandler{
 			return(false);
 		}
 	}
-		
+
 	private class LST_Thread implements Runnable{
 		private Socket tempSock;
 		private Scanner in;
-		
-		public void run(){			
+
+		public void run(){
 			//System.out.println("[+] in \"ConHandler\" private class \"LST_Thread\" run method");
-			
-			while(true){					
+
+			while(true){
 				try{
 					tempSock = srvSock.accept();
-					conArray.add(tempSock);				
-					
+					conArray.add(tempSock);
+
 					PrintWriter out = new PrintWriter(tempSock.getOutputStream());
 					in = new Scanner(tempSock.getInputStream());
 					String uname = null;
-					
+
 					out.print("Enter a user name (10 char max)\n\r> ");
 					out.flush();
-					
+
 					while(uname == null){
 						if(in.hasNext()){
 							uname = in.nextLine();
-							
+
 							if(uname.length() > 10){
 								unameArray.add(uname.substring(0,9));
 							}
 							else{
 								unameArray.add(uname);
 							}
-							
+
 							//send unameArray to all connected clients
 							for(int i = 0; i < conArray.size(); i++){
 								if(!conArray.get(i).isClosed()){
 									try {
 										PrintWriter outTemp = new PrintWriter(conArray.get(i).getOutputStream());
-										
+
 										for(int x = 0; x < unameArray.size(); x++){
-											outTemp.println(unameArray.get(x));									
+											outTemp.println(unameArray.get(x));
 											outTemp.flush();
 										}
-									} 
+									}
 									catch (IOException e){
 										// TODO Auto-generated catch block
 										e.printStackTrace();
@@ -100,69 +100,70 @@ public class ConHandler{
 							}
 						}
 					}
-					
+
 					//
-				} 
+				}
 				catch (IOException e){
 					e.printStackTrace();
-				}	
-				
+				}
+
 				// Create and start the receive thread
 				RCV_Thread cThread = new RCV_Thread(tempSock);
 				Thread y = new Thread(cThread);
 				y.start();
 
-				//TODO 
-				// find a graceful way to shutdown.				
+				//TODO
+				// find a graceful way to shutdown.
 			}
-		}		
+		}
 	}
-	
-	private class RCV_Thread implements Runnable{	
+
+	private class RCV_Thread implements Runnable{
 		private volatile Socket sock;
 		private volatile Scanner input;
 		private volatile PrintWriter clientOut;
+		final String SYS_INIT = "#!010101";
 		private Boolean run = true;
-		
-		public RCV_Thread(Socket s){						
+
+		public RCV_Thread(Socket s){
 			try{
 				//this.input = new Scanner(PrintWriter(tempSock));
 				input = new Scanner(s.getInputStream());
 				clientOut = new PrintWriter(s.getOutputStream());
 				sock = s;
-			} 
+			}
 			catch (IOException e){
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
-		
+
 		public void run(){
 			//System.out.println("[+] In \"ConnHandler\" private class \"RCV_Thread\" method \"run\"");
-									
+
 			while(run){
 				String sender = "";
-				
+
 				if(input.hasNext()){
 					String tempStr = input.nextLine();
-					
-					if(tempStr.contains("#!010101")){
+
+					if(tempStr.contains(SYS_INIT)){
 						parseSysArg(tempStr);
 					}
-					else{					
+					else{
 						for(int i = 0; i < conArray.size(); i++){
 							if(conArray.get(i).equals(sock)){
 								sender = unameArray.get(i);
 							}
 						}
-						for(int i = 0; i < conArray.size(); i++){							
+						for(int i = 0; i < conArray.size(); i++){
 							try{
 								PrintWriter out = new PrintWriter(conArray.get(i).getOutputStream());
-															
+
 								out.print(sender+": ");
 								out.println(tempStr);
 								out.flush();
-							} 
+							}
 							catch (IOException e){
 								e.printStackTrace();
 							}
@@ -170,23 +171,27 @@ public class ConHandler{
 					}
 				}
 			}
-		}//end method run	
-		
+		}//end method run
+
 		private int parseSysArg(String str){
+			//final String SYS_INIT = "#!010101";
+			final String SYS_USERS = "#!010101users";
+			final String SYS_LOGOUT = "#!010101logout";
+
 			System.out.println("[+] System request detected: "+str);
-			
-			if(str.contentEquals("#!010101users")){												
+
+			if(str.contentEquals(SYS_USERS)){
 				for(int x = 0; x < unameArray.size(); x++){
 					System.out.println("[+] Sending user array.");
-					clientOut.println(unameArray.get(x));									
+					clientOut.println(unameArray.get(x));
 					clientOut.flush();
 				}
-				
+
 				return 0;
 			}
-			else if(str.contentEquals("#!010101logout")){
+			else if(str.contentEquals(SYS_LOGOUT)){
 				System.out.print("[+] Logging out user: ");
-				
+
 				for(int i = 0; i < conArray.size(); i++){
 					if(conArray.get(i) == sock){
 						System.out.println(unameArray.get(i));
@@ -198,7 +203,7 @@ public class ConHandler{
 						run = false; //shuts the LST_Thread down
 					}
 				}
-				
+
 				return 0;
 			}
 			else if(str.contentEquals("#!010101")){
@@ -209,7 +214,7 @@ public class ConHandler{
 				//Placeholder
 				return 0;
 			}
-			
+
 			System.err.println("[!] Unable to parse system argument: "+str);
 			return 1;
 		}
--- Server.java
+++ Server.java
@@ -1,75 +1,92 @@
-package server;
-
-/* Project: xer0Chat
- * Author:  nu11xer0
- * Date:	25 May 2017
- * Purpose: A simple irc-like chat server
- */
-
 import java.io.IOException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-
-public class Server{	
+public class Server{
 	private static ConHandler handler;
-	
 
 	public static void main(String[] args){
 		int choice = 0;
-		
-		//System.out.println("[+] In class \"Server\" method \"main\"");
-		
-		try { 
-			handler = new ConHandler(); 
-		}
-		catch(Exception e){
-			e.printStackTrace();
-			System.exit(1);
-		}	
-		
+
+		parseArgs(args);
+
 		printMenu();
-		
-		while(true){	
+
+		while(true){
 			Scanner consoleInput = new Scanner(System.in);
-			
-			try{				
+
+			try{
 				choice = consoleInput.nextInt();
-				consoleInput.nextLine();
-														
+				consoleInput.nextLine(); //absorb the /n in the Scanner
+
 				switch(choice){
 					case 99: System.out.println("[+] Exiting...");
-							 try{ 
+							 try{
 								 if(handler.killSock()){
 									 System.out.println("[+] Server shutdown successfully. Bye!");
 								 }
 								 else{
 									 System.err.println("[!] Something went wrong while stopping the server.");
 								 }
-							 } 
-							 catch (IOException e) 
-							 {	e.printStackTrace(); }
+							 }
+							 catch (IOException e){
+								 e.printStackTrace(); 
+							 }
 							 consoleInput.close();
-							 System.exit(0); 
+							 System.exit(0);
 							 break;
-							 
+
 					default: System.out.println("[+] Invalid input, try again.\n");
 							 printMenu();
 							 break;
-				}				
+				}
 			}
 			catch(InputMismatchException e){
 				 System.out.println("[+] Ass sphincter says \"What\"?\n");
 				 printMenu();
 			}
-		}		
+		}
 	}//end method main
-	
+
+	private static void parseArgs(String[] args){
+		if(args.length > 0){
+			Boolean next = false;
+			
+			for(String s: args){								
+				if(next){					
+					try{
+						handler = new ConHandler(Integer.parseInt(s));
+						System.out.println("Server port: "+s);
+					}
+					catch(Exception e){
+						e.printStackTrace();
+						System.exit(1);
+					}					
+				}
+				else if(s.equals("-p")){
+					next = true;
+				}
+			}
+		}
+		else{
+			try{
+				handler = new ConHandler();
+				System.out.println("Server port: 4444");
+			}
+			catch(Exception e){
+				e.printStackTrace();
+				System.exit(1);
+			}
+		}
+		
+		return;
+	}
+
 	public static void printMenu(){
 		System.out.printf("%s%n%n%s%n%n%s",
 						  "_-= xeroChat Server Console =-_",
 						  "type 99 <enter> to quit.",
-						  "> ");		
+						  "> ");
 		return;
 	}//end method printMenu
 }
