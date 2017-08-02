41e41aff6fd30b7d739b8b7ae8927678c8546ec6
Aaron <soribnoraa@gmail.com>
2017-08-01 20:52:28 -0400

    File created
    Holds some constants (this file willl grow)

---
+++ Resources/Constants.java
@@ -0,0 +1,8 @@
+package Resources;
+
+public class Constants {
+	public final static int DEFAULT_PORT =  4444;
+	public final static String SYS_INIT =   "#!010101";
+	public final static String SYS_USERS =  "#!010101users";
+	public final static String SYS_LOGOUT = "#!010101logout";
+}
